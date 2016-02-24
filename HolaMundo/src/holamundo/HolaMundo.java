3bd83db0a1a7fe29f7140b27881c40e251f965b9
AlejandraMarin13 <jamarin393@misena.edu.co>
2016-02-23 21:02:04 -0500

    resta

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -15,14 +15,18 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+
         System.out.println("Hola Mundo");
-        
-        int a =5 ;
-        int b = 10 ;
-        int  resultado = a + b ;
-        
-        System.out.println("la suma entre a y b es :"+ resultado);
+
+        int a = 5;
+        int b = 10;
+        int resultado = a + b;
+
+        System.out.println("la suma entre a y b es :" + resultado);
+
+        int resultado2 = a - b;
+
+        System.out.println("la resta de a y b es :" + resultado2);
     }
-    
+
 }
